01d7fa2f9336591fbcdf2b801af0e9490bc73ea5
tokyo4j <hrak1529@gmail.com>
2024-02-11 13:37:38 +0900

    Polish sample gen

--- gen_sample.js
+++ gen_sample.js
@@ -6,8 +6,8 @@ if (!fs.existsSync("./test.db")) {
   db.serialize(() => {
     db.run("CREATE TABLE keys(user_code INT, timestamp INT, count INT)");
 
-    const start = new Date(2024, 1, 11, 7).getTime();
-    const end = new Date(2024, 1, 11, 20).getTime();
+    let start = new Date(2024, 1, 11, 7).getTime();
+    let end = new Date(2024, 1, 11, 20).getTime();
     for (let i = start; i < end; i += 5 * 60 * 1000) {
       db.run(
         "INSERT INTO keys VALUES(?, ?, ?)",
@@ -25,27 +25,44 @@ if (!fs.existsSync("./test.db")) {
     db.run(
       "CREATE TABLE sites(user_code INT, name VARCHAR(64), start INT, end INT)"
     );
-    db.run(
-      "INSERT INTO sites VALUES(?, ?, ?, ?)",
-      999,
-      "twitter.com",
-      new Date(2024, 1, 11, 7, 12).getTime(),
-      new Date(2024, 1, 11, 8, 23).getTime()
-    );
-    db.run(
-      "INSERT INTO sites VALUES(?, ?, ?, ?)",
-      999,
-      "youtube.com",
-      new Date(2024, 1, 11, 12, 34).getTime(),
-      new Date(2024, 1, 11, 13, 45).getTime()
-    );
+    start = new Date(2023, 1, 3).getTime();
+    end = new Date(2023, 1, 11).getTime();
+    for (
+      let i = start;
+      i <= end;
+      i += Math.floor(Math.random() * 3600000 * 12)
+    ) {
+      let j = i + Math.floor(Math.random() * 3600000 * 2);
+      db.run(
+        "INSERT INTO sites VALUES(?, ?, ?, ?)",
+        999,
+        Math.random() < 0.5 ? "twitter.com" : "youtube.com",
+        i,
+        j
+      );
+      i = j;
+    }
 
     db.run("CREATE TABLE sleeps(user_code INT, start INT, end INT)");
-    db.run(
-      "INSERT INTO sleeps VALUES(?, ?, ?)",
-      999,
-      new Date(2024, 1, 10, 23, 55).getTime(),
-      new Date(2024, 1, 11, 6, 45).getTime()
-    );
+    for (let i = 3; i <= 10; i++) {
+      db.run(
+        "INSERT INTO sleeps VALUES(?, ?, ?)",
+        999,
+        new Date(
+          2024,
+          1,
+          i,
+          20 + Math.floor(Math.random() * 6),
+          Math.floor(Math.random() * 60)
+        ).getTime(),
+        new Date(
+          2024,
+          1,
+          i + 1,
+          4 + Math.floor(Math.random() * 6),
+          Math.floor(Math.random() * 60)
+        ).getTime()
+      );
+    }
   });
 }
